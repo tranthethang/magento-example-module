0764cbcc4e68ab0d5b8088f5c59b1ad537392a4b
= <=>
2018-06-25 18:07:37 +0700

    create form add

--- Block/Index/Index.php
+++ Block/Index/Index.php
@@ -6,4 +6,22 @@ namespace Gentoma\Helpdesk\Block\Index;
 class Index extends \Magento\Framework\View\Element\Template
 {
 
+    const BEM_BLOCK = 'g-frm_helpdesk--create';
+
+    public function bemGetBlock()
+    {
+        return self::BEM_BLOCK;
+    }
+
+    public function bemGetElementClass($element = '', $state = [], $extra = [])
+    {
+        if (empty($element)) {
+            return '';
+        }
+
+        $class = [self::BEM_BLOCK . '__' . $element];
+
+        return implode(' ', $class);
+    }
+
 }
